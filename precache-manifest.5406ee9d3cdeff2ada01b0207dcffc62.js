dbecbe03cc6aa7140d5494a2a5198e39f37492ee
eleung32 <52718704+eleung32@users.noreply.github.com>
2020-03-26 23:41:57 -0400

    Updates

---
+++ precache-manifest.5406ee9d3cdeff2ada01b0207dcffc62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7aa1ae97d2ae0c3fb889adcaec03cd",
+    "url": "/robofriends_reactjs/index.html"
+  },
+  {
+    "revision": "0ef0e02b25f1b8a0f041",
+    "url": "/robofriends_reactjs/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e0bb1f9da3cd414300ca",
+    "url": "/robofriends_reactjs/static/css/main.08293ea7.chunk.css"
+  },
+  {
+    "revision": "0ef0e02b25f1b8a0f041",
+    "url": "/robofriends_reactjs/static/js/2.576d87f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends_reactjs/static/js/2.576d87f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bb1f9da3cd414300ca",
+    "url": "/robofriends_reactjs/static/js/main.457b49c7.chunk.js"
+  },
+  {
+    "revision": "1c1bb667b0077606a881",
+    "url": "/robofriends_reactjs/static/js/runtime-main.8e6df2e6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends_reactjs/static/media/SEGA.12a3db8b.woff"
+  }
+]);
